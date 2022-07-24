70d31c648ef65594975c45c427fef0f98b4dc1c0
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-07-24 16:17:06 -0300

    feat: adicionando middleware de validação de payload na rota

--- routes.js
+++ routes.js
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv'
 import express from 'express';
 import { CreateUserController } from './src/users/controllers/createUserController.js';
-
+import {createUserValidator} from './src/users/validators/createUserValidator.js'
+import { validatePayloadMiddleware } from './src/users/middlewares/payloadValidatorMiddleware.js';
 const routes = express.Router();
 
 const createUser = new CreateUserController();
@@ -11,7 +12,7 @@ routes.get("/", (req, res) => {
     res.send(`Hi Node and Docker!!! ${applicationName}`);
   });
 
-routes.post('/user',(req, res) => {
+routes.post('/user', createUserValidator, validatePayloadMiddleware, (req, res) => {
     createUser.handle(req, res);
 });
 
