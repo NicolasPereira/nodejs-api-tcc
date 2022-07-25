8d3b9665c3cb57de2cdcbd4419f439b55f3cf6b0
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-07-25 10:06:50 -0300

    fix: removendo console.log

--- src/users/utils/encrypt.js
+++ src/users/utils/encrypt.js
@@ -1,7 +1,6 @@
 import bcrypt from 'bcrypt'
 
 export async function encryptPassword(password){
-    console.log(process.env.SALT);
     const salt = await bcrypt.genSalt(Number(process.env.SALT));
     return await bcrypt.hash(password, salt);
 }
