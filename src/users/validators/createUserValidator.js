cb66af65747642fdf31f8c0566f41bcd71f7ff42
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-07-30 11:32:05 -0300

    feat: adicionando o middleware na validação

--- src/users/validators/createUserValidator.js
+++ src/users/validators/createUserValidator.js
@@ -1,4 +1,5 @@
 import {body} from 'express-validator'
+import { validatePayloadMiddleware } from '../middlewares/payloadValidatorMiddleware.js'
 
 const createUserValidator = 
     [
@@ -6,7 +7,8 @@ const createUserValidator =
         body('email').notEmpty().withMessage('O campo email é obrigatório'),
         body('email').isEmail().withMessage('O campo email esta incorreto'),
         body('password').notEmpty().withMessage('O campo senha é obrigatório'),
-        body('password').isLength({min: 5}).withMessage('A senha deve ter no minimo 5 caracteres')
+        body('password').isLength({min: 5}).withMessage('A senha deve ter no minimo 5 caracteres'),
+        validatePayloadMiddleware
     ];
 
 export {createUserValidator}
