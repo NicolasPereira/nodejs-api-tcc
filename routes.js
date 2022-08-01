cb956fe8adcd4d5c2c8701c0238234ab4690214b
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-07-31 23:07:56 -0300

    feat: criando rota para login

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv'
 import express from 'express';
 import { CreateUserController } from './src/users/controllers/createUserController.js';
+import { AuthController } from './src/auth/controllers/authController.js'
 import {createUserValidator} from './src/users/validators/createUserValidator.js'
 const routes = express.Router();
 
 const createUser = new CreateUserController();
+const authUserController = new AuthController(); 
 
 routes.get("/", (req, res) => {
     const applicationName = process.env.APP_NAME;
@@ -12,5 +14,7 @@ routes.get("/", (req, res) => {
   });
 
 routes.post('/user', createUserValidator, createUser.handle);
+
+routes.post('/login', authUserController.authenticate);
 
 export { routes };
