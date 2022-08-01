3ac06b1daaf3c2dfee4cb4aa25d40e483fa7ae0d
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-07-31 23:56:00 -0300

    feat: add validate payload

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ import express from 'express';
 import { CreateUserController } from './src/users/controllers/createUserController.js';
 import { AuthController } from './src/auth/controllers/authController.js'
 import {createUserValidator} from './src/users/validators/createUserValidator.js'
+import {loginValidator} from './src/auth/validators/loginValidator.js'
 const routes = express.Router();
 
 const createUser = new CreateUserController();
@@ -15,6 +16,6 @@ routes.get("/", (req, res) => {
 
 routes.post('/user', createUserValidator, createUser.handle);
 
-routes.post('/login', authUserController.authenticate);
+routes.post('/login', loginValidator, authUserController.authenticate);
 
 export { routes };
