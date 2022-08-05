88a1bef3b49c76cf437a3ddbcecd9d3b865bd20d
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-08-05 11:04:20 -0300

    feat: alterando tratamento de erro

--- src/shared/middlewares/authMiddleware.js
+++ src/shared/middlewares/authMiddleware.js
@@ -3,12 +3,12 @@ import { validateToken } from "../auth/validateToken.js"
 const authMiddleware = (req, res, next) => {
 
     const token = req.headers['authorization'];
-    if (!token) return res.status(401).json({ auth: false, message: 'Token de autenticação não fornecido' });
+    if (!token) return res.status(401).json({ error: true, message: 'Token de autenticação não fornecido' });
     
     const result = validateToken(token);
 
-    if(!result) {
-        return res.status(401).json({ auth: false, message: 'Token de autenticação não é válido' })
+    if(result.messageError) {
+        return res.status(401).json({ error: true, message: result.messageError })
     }
 
     req.userId = result.user.id;
