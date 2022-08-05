1093921c166cb0cba00e5f733cbb0f79609f4723
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-08-05 11:04:02 -0300

    feat: alterando retorno do método validateToken

--- src/shared/auth/validateToken.js
+++ src/shared/auth/validateToken.js
@@ -2,8 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export function validateToken(token) {
     return jwt.verify(token, process.env.SECRET, function(err, decoded) {
-        if (err) return false;
-        
-        return decoded;
+      const errorMessage = { messageError: 'Token de autenticação não é válido' };
+      return (err) ? errorMessage : decoded;  
       });
 }
