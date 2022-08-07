befac7cca5b55b7f2b07494dbc2e30a9f02d2abb
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-08-07 15:10:59 -0300

    [Feature]  - Adicionando arquivo de config (#10)
    * feat: criando arquivo de config
    
    * feat: alterando valor da env para valor do config
    
    * importando config

--- src/shared/auth/validateToken.js
+++ src/shared/auth/validateToken.js
@@ -1,7 +1,8 @@
 import jwt from 'jsonwebtoken';
+import { config } from '../../shared/config/config.js';
 
 export function validateToken(token) {
-    return jwt.verify(token, process.env.SECRET, function(err, decoded) {
+    return jwt.verify(token,config.SECRET, function(err, decoded) {
       const errorMessage = { messageError: 'Token de autenticação não é válido' };
       return (err) ? errorMessage : decoded;  
       });
--- src/users/utils/encrypt.js
+++ src/users/utils/encrypt.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt'
+import { config } from '../../shared/config/config.js';
 
 export async function encryptPassword(password){
-    const salt = await bcrypt.genSalt(Number(process.env.SALT));
+    const salt = await bcrypt.genSalt(Number(config.SALT));
     return await bcrypt.hash(password, salt);
 }
