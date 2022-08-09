c6db39127802538d9ff11e1fac5fd6de0243870a
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-08-09 16:54:18 -0300

    [Feature] - Buscar informações do usuário autenticado (#11)
    * feat: criação do Controller para buscar usuário autenticado
    
    * feat: criação do repository
    
    * feat: criação da rota
    
    * fix: corrigindo o await
    
    * feat: mudando rota
    
    * feat: retornando itens do recurso

--- routes.js
+++ routes.js
@@ -1,28 +1,31 @@
-import dotenv from 'dotenv'
-import express from 'express';
-import { CreateUserController } from './src/users/controllers/createUserController.js';
-import { AuthController } from './src/auth/controllers/authController.js'
-import {createUserValidator} from './src/users/validators/createUserValidator.js'
-import {loginValidator} from './src/auth/validators/loginValidator.js'
-import { authMiddleware } from './src/shared/middlewares/authMiddleware.js';
-import { config } from './src/shared/config/config.js';
+import dotenv from "dotenv";
+import express from "express";
+import { AuthController } from "./src/auth/controllers/authController.js";
+import { CreateUserController } from "./src/users/controllers/createUserController.js";
+import { MeController } from "./src/users/controllers/MeController.js";
+import { createUserValidator } from "./src/users/validators/createUserValidator.js";
+import { loginValidator } from "./src/auth/validators/loginValidator.js";
+import { authMiddleware } from "./src/shared/middlewares/authMiddleware.js";
+import { config } from "./src/shared/config/config.js";
 
 const routes = express.Router();
 
 const createUser = new CreateUserController();
-const authUserController = new AuthController(); 
+const authUserController = new AuthController();
+const meController = new MeController();
 
 routes.post("/", authMiddleware, (req, res) => {
-    const applicationName = config.APP_NAME;
-    
-    res.json({
-      "message" : `Hi Node and Docker!!! ${applicationName} - ${req.userName}`
-    });
+  const applicationName = config.APP_NAME;
 
+  res.json({
+    message: `Hi Node and Docker!!! ${applicationName} - ${req.userName}`,
   });
+});
 
-routes.post('/user', createUserValidator, createUser.handle);
+routes.post("/users", createUserValidator, createUser.handle);
 
-routes.post('/login', loginValidator, authUserController.authenticate);
+routes.get("/users/me", authMiddleware, meController.handle);
+
+routes.post("/login", loginValidator, authUserController.authenticate);
 
 export { routes };
---
+++ src/users/controllers/MeController.js
@@ -0,0 +1,20 @@
+import { findUserById } from "../repository/findUserByIdRepository.js";
+
+export class MeController {
+  async handle(req, res) {
+    const user = await findUserById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({
+        error: true,
+        message: "Usuário não encontrado",
+      });
+    }
+
+    return res.status(200).json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  }
+}
