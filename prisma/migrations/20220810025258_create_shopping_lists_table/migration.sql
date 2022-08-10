bf6d6d2850b58229364d19725c3c0295c5ce4a98
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-08-09 23:55:13 -0300

    feat: add migration

---
+++ prisma/migrations/20220810025258_create_shopping_lists_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "shopping_lists" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "title" VARCHAR(255) NOT NULL,
+    "content" TEXT,
+    "finished" BOOLEAN NOT NULL DEFAULT false,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "shopping_lists_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "shopping_lists" ADD CONSTRAINT "shopping_lists_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
