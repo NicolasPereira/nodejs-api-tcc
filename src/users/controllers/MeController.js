d3c32d39f813b0cac01d4f1fdac8a708fadbfb43
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-08-10 23:59:23 -0300

    feat: implements http-status-code in meController

--- src/users/controllers/MeController.js
+++ src/users/controllers/MeController.js
@@ -1,17 +1,18 @@
 import { findUserById } from "../repository/findUserByIdRepository.js";
+import { StatusCodes } from "http-status-codes";
 
 export class MeController {
   async handle(req, res) {
     const user = await findUserById(req.userId);
 
     if (!user) {
-      return res.status(404).json({
+      return res.status(StatusCodes.NOT_FOUND).json({
         error: true,
         message: "Usuário não encontrado",
       });
     }
 
-    return res.status(200).json({
+    return res.status(StatusCodes.OK).json({
       id: user.id,
       name: user.name,
       email: user.email,
