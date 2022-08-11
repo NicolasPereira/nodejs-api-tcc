2dd347d906e6ea5de6870e2e261524881003ee21
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-08-10 23:58:36 -0300

    feat: implements http-status-code in authMiddleware

--- src/shared/middlewares/authMiddleware.js
+++ src/shared/middlewares/authMiddleware.js
@@ -1,21 +1,25 @@
-import { validateToken } from "../auth/validateToken.js"
+import { validateToken } from "../auth/validateToken.js";
+import { StatusCodes } from "http-status-codes";
 
 const authMiddleware = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token)
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ error: true, message: "Token de autenticação não fornecido" });
 
-    const token = req.headers['authorization'];
-    if (!token) return res.status(401).json({ error: true, message: 'Token de autenticação não fornecido' });
-    
-    const result = validateToken(token);
+  const result = validateToken(token);
 
-    if(result.messageError) {
-        return res.status(401).json({ error: true, message: result.messageError })
-    }
+  if (result.messageError) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ error: true, message: result.messageError });
+  }
 
-    req.userId = result.user.id;
-    req.userName = result.user.name;
+  req.userId = result.user.id;
+  req.userName = result.user.name;
 
-    next();
-    
-}
+  next();
+};
 
-export { authMiddleware }
+export { authMiddleware };
