df21773590f9be2eb77b4394a56af907aefc14e5
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-09-09 14:55:43 -0300

    feat: add port

--- src/shared/config/config.js
+++ src/shared/config/config.js
@@ -1,5 +1,6 @@
 export const config = {
-    APP_NAME: process.env.APP_NAME || "TCC",
-    SALT: process.env.SALT || 10,
-    SECRET: process.env.SECRET || mysecret,
-}
+  APP_NAME: process.env.APP_NAME || "TCC",
+  SALT: process.env.SALT || 10,
+  SECRET: process.env.SECRET || mysecret,
+  PORT: process.env.PORT || 5000,
+};
