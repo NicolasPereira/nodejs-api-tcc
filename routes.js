9dd721eb752b0370638997e4eaf5185da6ecb060
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-09-09 00:47:13 -0300

    feat: criação da rota

--- routes.js
+++ routes.js
@@ -11,6 +11,7 @@ import { authMiddleware } from "./src/shared/middlewares/authMiddleware.js";
 import { createShoppingListProductsValidator } from "./src/shoppingListProducts/validators/createProductsValidator.js";
 import { config } from "./src/shared/config/config.js";
 import { ShoppingListProductsController } from "./src/shoppingListProducts/controllers/ShoppingListProductsController.js";
+import { FindShoppingListByUserController } from "./src/shoppingList/controllers/FindShoppingListByUserController.js";
 const routes = express.Router();
 
 const createUser = new CreateUserController();
@@ -18,6 +19,7 @@ const authUserController = new AuthController();
 const meController = new MeController();
 const createShoppingList = new CreateShoppingListController();
 const shoppingListProductsController = new ShoppingListProductsController();
+const findShoppingListByUser = new FindShoppingListByUserController();
 
 routes.post("/", authMiddleware, (req, res) => {
   const applicationName = config.APP_NAME;
@@ -30,6 +32,12 @@ routes.post("/", authMiddleware, (req, res) => {
 routes.post("/users", createUserValidator, createUser.handle);
 
 routes.get("/users/me", authMiddleware, meController.handle);
+
+routes.get(
+  "/users/me/shopping-list",
+  authMiddleware,
+  findShoppingListByUser.handle
+);
 
 routes.post("/login", loginValidator, authUserController.authenticate);
 
