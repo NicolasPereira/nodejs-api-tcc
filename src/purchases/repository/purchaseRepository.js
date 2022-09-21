052697f8152f1cdef53402726c6930a40c96985f
Nicolas Pereira <pereira.nicolasrocha@gmail.com>
2022-09-21 01:45:03 -0300

    feat: criando método para procurar compras do user lgoado

--- src/purchases/repository/purchaseRepository.js
+++ src/purchases/repository/purchaseRepository.js
@@ -9,4 +9,22 @@ async function createPurchase(title, userId) {
   });
 }
 
-export { createPurchase };
+async function findPurchasesByUserId(userId) {
+  return prismaClient.purchases.findMany({
+    orderBy: [
+      {
+        id: "desc",
+      },
+    ],
+    where: {
+      userId: userId,
+    },
+    select: {
+      id: true,
+      title: true,
+      createdAt: true,
+    },
+  });
+}
+
+export { createPurchase, findPurchasesByUserId };
